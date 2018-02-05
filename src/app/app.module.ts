6367d5c31decc18192b28d3e4434bbce15747d3a
Andrey Murin <96467840@mail.ru>
2018-02-05 07:18:39 +0300

    роуты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from '../pages/home.component';
+import { NotFoundComponent } from '../pages/not-found.component';
 
+// определение маршрутов
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  //{ path: 'about', component: AboutComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent/*, AboutComponent*/, NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
